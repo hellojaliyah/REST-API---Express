8a5a3fa9919779253bf2a662770916571184d9a9
Jaliyah <jaliyahdharrison@gmail.com>
2020-02-03 01:10:47 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const Joi = require('joi')
+const logger = require('./logger')
+const express = require('express')
+const app = express();
+const helmet = require('helmet')
+const morgan = require('morgan')
+
+
+app.use(express.json())  //to use req.body
+app.use(express.urlencoded({extended: true})) //parses income req key value pairs
+app.use(express.static('public')) //static assets go here
+app.use(helmet())
+
+if(app.get('env') === 'development') {
+app.use(morgan('tiny'))
+console.log('Morgan enabled...')
+}
+
+
+app.use(logger)
+
+const courses = [
+    {id: 1, name: 'course1'},
+    {id: 2, name: 'course2'},
+    {id: 3, name: 'course3'},
+];
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+});
+
+app.get('/api/courses', (req, res) => {
+    res.send(courses)
+})
+
+
+app.listen(5000)
